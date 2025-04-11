333d761c1774d0046792a0c2b217cfa5ecafd1c0
Felipe Langoni Ramos <felipelangoniramos@pop-os.localdomain>
2025-04-11 20:55:51 -0300

    feat: Static Evaluation Utility Function and Peace Square Table working

---
+++ noisyboy.cpp
@@ -0,0 +1,213 @@
+#include <iostream>
+#include <chess.hpp>
+#include <map>
+#include <vector>
+#include <unordered_map>
+
+using namespace chess;
+
+std::vector<int> mirrorTable(const std::vector<int>& original) {
+    if (original.size() != 64) {
+        std::cerr << "mirrorTable() error: input vector size != 64\n";
+        return std::vector<int>(64, 0);
+    }
+    std::vector<int> mirrored(64);
+    for (int row = 0; row < 8; ++row) {
+        for (int col = 0; col < 8; ++col) {
+            int srcIndex = row * 8 + col;
+            int destIndex = (7 - row) * 8 + col;
+            mirrored[destIndex] = original[srcIndex];
+        }
+    }
+    return mirrored;
+}
+
+struct PieceTable {
+    std::unordered_map<std::string, std::vector<int>> tables;
+
+    PieceTable() {
+        // Pawn table
+        tables["p"] = {
+            0, 0, 0, 0, 0, 0, 0, 0,
+            5, 10, 10, -40, -40, 10, 10, 5,
+            5, -5, -10, 0, 0, -10, -5, 5,
+            0, 0, 0, 50, 50, 0, 0, 0,
+            5, 5, 10, 25, 25, 10, 5, 5,
+            10, 10, 20, 30, 30, 20, 10, 10,
+            50, 50, 50, 50, 50, 50, 50, 50,
+            0, 0, 0, 0, 0, 0, 0, 0
+        };
+
+        // Knight table
+        tables["n"] = {
+            -50, -40, -30, -30, -30, -30, -40, -50,
+            -40, -20, 0, 5, 5, 0, -20, -40,
+            -30, 5, 10, 15, 15, 10, 5, -30,
+            -30, 0, 15, 20, 20, 15, 0, -30,
+            -30, 5, 15, 20, 20, 15, 5, -30,
+            -30, 0, 10, 15, 15, 10, 0, -30,
+            -40, -20, 0, 0, 0, 0, -20, -40,
+            -50, -40, -30, -30, -30, -30, -40, -50
+        };
+
+        // Bishop table
+        tables["b"] = {
+            -20, -10, -40, -10, -10, -40, -10, -20,
+            -10, 5, 0, 0, 0, 0, 5, -10,
+            -10, 10, 10, 10, 10, 10, 10, -10,
+            -10, 0, 20, 10, 10, 20, 0, -10,
+            -10, 5, 5, 10, 10, 5, 5, -10,
+            -10, 0, 5, 10, 10, 5, 0, -10,
+            -10, 0, 0, 0, 0, 0, 0, -10,
+            -20, -10, -40, -10, -10, -40, -10, -20
+        };
+
+        // Rook table
+        tables["r"] = {
+            0, 0, 0, 5, 5, 0, 0, 0,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            5, 10, 10, 10, 10, 10, 10, 5,
+            0, 0, 0, 0, 0, 0, 0, 0
+        };
+
+        // Queen table
+        tables["q"] = {
+            -20, -10, -10, -5, -5, -10, -10, -20,
+            -10, 0, 0, 0, 0, 0, 0, -10,
+            -10, 5, 5, 5, 5, 5, 0, -10,
+            0, 0, 5, 5, 5, 5, 0, -5,
+            -5, 0, 5, 5, 5, 5, 0, -5,
+            -10, 0, 5, 5, 5, 5, 0, -10,
+            -10, 0, 0, 0, 0, 0, 0, -10,
+            -20, -10, -10, -5, -5, -10, -10, -20
+        };
+
+        // King table
+        tables["k"] = {
+            20, 30, 10, 0, 0, 10, 30, 20,
+            20, 20, -10, -10, -10, -10, 20, 20,
+            -10, -20, -20, -20, -20, -20, -20, -10,
+            -20, -30, -30, -40, -40, -30, -30, -20,
+            -30, -40, -40, -50, -50, -40, -40, -30,
+            -30, -40, -40, -50, -50, -40, -40, -30,
+            -30, -40, -40, -50, -50, -40, -40, -30,
+            -30, -40, -40, -50, -50, -40, -40, -30
+        };
+        tables["P"] = mirrorTable(tables["p"]);
+        tables["N"] = mirrorTable(tables["n"]);
+        tables["B"] = mirrorTable(tables["b"]);
+        tables["R"] = mirrorTable(tables["r"]);
+        tables["Q"] = mirrorTable(tables["q"]);
+        tables["K"] = mirrorTable(tables["k"]);
+
+    }
+};
+
+int pst_score(Board &board)
+{
+    int score = 0;
+    score += board.pieces(PieceType::PAWN,Color::WHITE).count() * 1;
+    score += board.pieces(PieceType::KNIGHT,Color::WHITE).count() * 3;
+    score += board.pieces(PieceType::BISHOP,Color::WHITE).count() * 3;
+    score += board.pieces(PieceType::ROOK,Color::WHITE).count() * 5;
+    score += board.pieces(PieceType::QUEEN,Color::WHITE).count() * 9;
+
+    score -= board.pieces(PieceType::PAWN,Color::BLACK).count() * 1;
+    score -= board.pieces(PieceType::KNIGHT,Color::BLACK).count() * 3;
+    score -= board.pieces(PieceType::BISHOP,Color::BLACK).count() * 3;
+    score -= board.pieces(PieceType::ROOK,Color::BLACK).count() * 5;
+    score -= board.pieces(PieceType::QUEEN,Color::BLACK).count() * 9;
+
+    return score;
+}
+
+int PieceSquares(Bitboard piece, std::string type) {
+    int score = 0;
+ 
+    for (int square = 0; square < 64; ++square) {
+        if ((piece >> square) & 1ULL) {
+            score += PieceTable().tables[type][square];
+        }
+    }
+    std::cout << std::endl;
+    return score;
+}
+
+int score(Board &board)
+{
+    int score = 0;
+    score += board.pieces(PieceType::PAWN,Color::WHITE).count() * 100;
+    score += board.pieces(PieceType::KNIGHT,Color::WHITE).count() * 300;
+    score += board.pieces(PieceType::BISHOP,Color::WHITE).count() * 300;
+    score += board.pieces(PieceType::ROOK,Color::WHITE).count() * 500;
+    score += board.pieces(PieceType::QUEEN,Color::WHITE).count() * 900;
+    std::cout << "White score: " << score << std::endl;
+
+    score -= board.pieces(PieceType::PAWN,Color::BLACK).count() * 100;
+    score -= board.pieces(PieceType::KNIGHT,Color::BLACK).count() * 300;
+    score -= board.pieces(PieceType::BISHOP,Color::BLACK).count() * 300;
+    score -= board.pieces(PieceType::ROOK,Color::BLACK).count() * 500;
+    score -= board.pieces(PieceType::QUEEN,Color::BLACK).count() * 900;
+    std::cout << "Black score: " << score << std::endl;
+
+    score += PieceSquares(board.pieces(PieceType::PAWN, Color::BLACK), "p");
+    score += PieceSquares(board.pieces(PieceType::KNIGHT, Color::BLACK), "n");
+    score += PieceSquares(board.pieces(PieceType::BISHOP, Color::BLACK), "b");
+    score += PieceSquares(board.pieces(PieceType::ROOK, Color::BLACK), "r");
+    score += PieceSquares(board.pieces(PieceType::QUEEN, Color::BLACK), "q");
+    score += PieceSquares(board.pieces(PieceType::KING, Color::BLACK), "k");
+    std::cout << "Black piece square score: " << score << std::endl;
+
+    score -= PieceSquares(board.pieces(PieceType::PAWN, Color::WHITE), "P");
+    score -= PieceSquares(board.pieces(PieceType::KNIGHT, Color::WHITE), "N");
+    score -= PieceSquares(board.pieces(PieceType::BISHOP, Color::WHITE), "B");
+    score -= PieceSquares(board.pieces(PieceType::ROOK, Color::WHITE), "R");
+    score -= PieceSquares(board.pieces(PieceType::QUEEN, Color::WHITE), "Q");
+    score -= PieceSquares(board.pieces(PieceType::KING, Color::WHITE), "K");
+    std::cout << "White piece square score: " << score << std::endl;
+    
+    std::cout << "Score: " << score << std::endl;
+
+    return score;
+}
+
+
+int negamax(Board &board)
+{
+    int score = 0;
+    score += board.pieces(PieceType::PAWN,Color::WHITE).count() * 1;
+    score += board.pieces(PieceType::KNIGHT,Color::WHITE).count() * 3;
+    score += board.pieces(PieceType::BISHOP,Color::WHITE).count() * 3;
+    score += board.pieces(PieceType::ROOK,Color::WHITE).count() * 5;
+    score += board.pieces(PieceType::QUEEN,Color::WHITE).count() * 9;
+
+    score -= board.pieces(PieceType::PAWN,Color::BLACK).count() * 1;
+    score -= board.pieces(PieceType::KNIGHT,Color::BLACK).count() * 3;
+    score -= board.pieces(PieceType::BISHOP,Color::BLACK).count() * 3;
+    score -= board.pieces(PieceType::ROOK,Color::BLACK).count() * 5;
+    score -= board.pieces(PieceType::QUEEN,Color::BLACK).count() * 9;
+
+    return score;
+}
+
+
+
+int main () {
+    Board board = Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+    Movelist moves;
+    movegen::legalmoves(moves, board);
+
+    for (const auto& move : moves) {
+        std::cout << move << " ";
+    }
+
+
+    std::cout << "Score: " << score(board) << std::endl;
+
+    return 0;
+}
